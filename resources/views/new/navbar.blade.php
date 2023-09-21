e208f3b0eecf4f440abc142112762f44c273d219
salma-mohamed-37 <lulymohamed37@gmail.com>
2023-09-21 20:55:02 +0300

    allow profile settings in profile only

--- resources/views/new/navbar.blade.php
+++ resources/views/new/navbar.blade.php
@@ -9,15 +9,17 @@
 
        @if (Route::has('login'))
         @auth
-            <input type=button value=Profile class=navbarButtons onclick="window.location.href=&quot;{{ route('profile.edit') }}&quot;" />
+            @if (Route::is('profilePosts'))
+                 <input type=button value="Profile settings" class=navbarButtons onclick="window.location.href=&quot;{{ route('profile.edit') }}&quot;" />
+            @endif
             <form method="POST" action="{{ route('logout') }}" style="display: inline-block; float: right;">
                 @csrf
-                <input type="submit" value='Log Out' class=navbarButtons />
+                <input type="submit" value='Log out' class=navbarButtons />
             </form>
             <input type=button value=Explore class=navbarButtons onclick="window.location.href=&quot;{{ route('dashboard') }}&quot;"/>
-            <input type=button value="Your posts" class=navbarButtons onclick="window.location.href=&quot;{{ route('profilePosts') }}&quot;"/>
+            <input type=button value="Your profile" class=navbarButtons onclick="window.location.href=&quot;{{ route('profilePosts') }}&quot;"/>
         @else
-            <input type=button value="Login" class=navbarButtons onclick="window.location.href=&quot;{{ route('login') }}&quot;" />
+            <input type=button value="Log in" class=navbarButtons onclick="window.location.href=&quot;{{ route('login') }}&quot;" />
             <input type=button value="Register" class=navbarButtons onclick="window.location.href=&quot;{{ route('register') }}&quot;"/>
         @endauth
     @endif
